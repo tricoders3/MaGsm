8d7f26da8b2ae04d8237abf19e82f226716114f3
ichraf khalfaoui <ichrafkhalfaoui1@gmail.cm>
2025-12-17 10:09:55 +0100

    feat: add login page UI

---
+++ frontend/src/screens/Login.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input type="email" placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/screens/Register.jsx
@@ -0,0 +1,20 @@
+const Register = () => {
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input type="text" placeholder="Full Name" />
+      <br />
+
+      <input type="email" placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Register</button>
+    </div>
+  );
+};
+
+export default Register;
