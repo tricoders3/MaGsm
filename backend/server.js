60adcbd8d6b1b918db3852a8792717461870c571
ichraf khalfaoui <ichrafkhalfaoui1@gmail.cm>
2026-02-06 10:38:14 +0100

    fix registerr

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,12 @@ app.use(
     secret: process.env.SESSION_SECRET || "fallbacksecret",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // obligatoire en HTTPS
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // nécessaire pour cookies cross-site
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
+    },
   })
 );
 
@@ -70,11 +75,14 @@ app.use(passport.session());
 // ------------------
 app.use(
   cors({
-    origin: ["https://magsm.onrender.com", "http://localhost:3000"],
+    origin: process.env.NODE_ENV === "production"
+      ? "https://magsm.onrender.com"
+      : "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 if (process.env.NODE_ENV === "production") {
   const buildPath = path.join(__dirname, "../frontend/build"); 
   app.use(express.static(buildPath));
