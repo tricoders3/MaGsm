807519be7f0a88a79afb38b4d18bf281552c244f
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-27 00:39:08 +0100

    Update redirect URL for OAuth success

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -139,9 +139,7 @@ export const facebookLoginSuccess = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
     });
 
-    res.redirect(
-      `${process.env.CLIENT_URL}/oauth-success?token=${accessToken}`
-    );
+  res.redirect(`https://magsm.tn/oauth-success?token=${accessToken}`);
   } catch (error) {
     if (error.message === "ACCOUNT_NOT_APPROVED") {
       return res.redirect(
