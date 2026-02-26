efc0487a3c042fea586c1fc8f56582b2fe693622
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-27 00:34:48 +0100

    Update OAuth redirect URL to use CLIENT_URL

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -105,9 +105,9 @@ export const googleLoginSuccess = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
     });
 
-    
-      res.redirect(`https://magsm.tn/oauth-success?token=${accessToken}`);
-  
+    res.redirect(
+      `${process.env.CLIENT_URL}/oauth-success?token=${accessToken}`
+    );
   } catch (error) {
     // ⛔ compte non approuvé
     if (error.message === "ACCOUNT_NOT_APPROVED") {
@@ -139,7 +139,9 @@ export const facebookLoginSuccess = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
     });
 
- res.redirect(`https://magsm.tn/oauth-success?token=${accessToken}`);
+    res.redirect(
+      `${process.env.CLIENT_URL}/oauth-success?token=${accessToken}`
+    );
   } catch (error) {
     if (error.message === "ACCOUNT_NOT_APPROVED") {
       return res.redirect(
@@ -151,7 +153,6 @@ export const facebookLoginSuccess = async (req, res) => {
     res.redirect(`${process.env.CLIENT_URL}/oauth-error`);
   }
 };
-
 /**
  * Logout
  */
