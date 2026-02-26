94b1ae7a266d06c9e0db3a153482d7a5501eac4e
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-26 23:16:48 +0100

    Change server listen address to 127.0.0.1

--- backend/server.js
+++ backend/server.js
@@ -119,4 +119,6 @@ if (process.env.NODE_ENV === "production") {
 // Start server
 // ------------------
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`Backend running on http://127.0.0.1:${PORT}`);
+});
