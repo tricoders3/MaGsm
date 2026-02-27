3eb7e59d65ee1f093ffa2d6ee8079c6632f8930d
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-27 03:59:46 +0100

    Update createAdmin.js

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import User from "./models/User.js"; // ton modèle User
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const createAdmin = async () => {
+  try {
+    const hashedPassword = await bcrypt.hash("20771717", 10); // mot de passe admin
+    const admin = new User({
+      name: "Admin",
+      email: "magsm2077@gmail.com",
+      password: hashedPassword,
+      role: "admin",
+      isApproved: true,   // ✅ admin approuvé
+      createdAt: new Date(),
+    });
+
+    await admin.save();
+    console.log("✅ Admin créé avec succès !");
+    process.exit();
+  } catch (err) {
+    console.error("❌ Erreur :", err.message);
+    process.exit(1);
+  }
+};
+
+createAdmin();
