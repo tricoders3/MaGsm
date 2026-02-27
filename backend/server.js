28f04a58472f66e6a2bc9c4b18bd4b87f61d8c80
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-27 02:54:20 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "./passport.js";
 import cors from "cors";
-
+import MongoStore from 'connect-mongo';
 // ------------------
 // Routes
 // ------------------
@@ -80,6 +80,12 @@ app.use(
   })
 );
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+}));
 // ------------------
 // Passport
 // ------------------
