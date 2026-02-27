bdc109322276d94e2a1541f25ca0df87e2c9b8f2
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-02-27 03:06:07 +0100

    Change server binding from 127.0.0.1 to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -125,6 +125,6 @@ if (process.env.NODE_ENV === "production") {
 // Start server
 // ------------------
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on http://127.0.0.1:${PORT}`);
 });
