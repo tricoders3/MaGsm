d1a8bd52e37fd0a184dcd41c4815d5f66a57cc08
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-03-26 15:49:06 +0100

    Fix product ID in API call for fetching product

--- frontend/src/screens/ProductDetails.jsx
+++ frontend/src/screens/ProductDetails.jsx
@@ -40,7 +40,7 @@ const { productName } = useParams();
     const fetchProductAndSimilar = async () => {
       try {
         const resProduct = await axios.get(
-          `${BASE_URL}/api/products/${currentProduct._id}`
+          `${BASE_URL}/api/products/${product._id}`
         );
         const currentProduct = resProduct.data;
         setProduct(currentProduct);
