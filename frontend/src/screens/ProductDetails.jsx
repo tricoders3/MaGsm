da09a4a8fa51fcbef69ab7d43d9adfd6d554502b
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-03-26 15:44:25 +0100

    Update ProductDetails.jsx

--- frontend/src/screens/ProductDetails.jsx
+++ frontend/src/screens/ProductDetails.jsx
@@ -40,7 +40,7 @@ const { productName } = useParams();
     const fetchProductAndSimilar = async () => {
       try {
         const resProduct = await axios.get(
-          `${BASE_URL}/api/products/${productId}`
+          `${BASE_URL}/api/products/${currentProduct._id}`
         );
         const currentProduct = resProduct.data;
         setProduct(currentProduct);
