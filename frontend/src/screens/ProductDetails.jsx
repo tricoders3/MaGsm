67ad9b4df9d8d53f95e35f09ff9755489214b1c1
chaimaTroudi <47435821+26884938@users.noreply.github.com>
2026-03-26 16:16:17 +0100

    Fetch product by name instead of ID

--- frontend/src/screens/ProductDetails.jsx
+++ frontend/src/screens/ProductDetails.jsx
@@ -40,7 +40,7 @@ const { productName } = useParams();
     const fetchProductAndSimilar = async () => {
       try {
         const resProduct = await axios.get(
-          `${BASE_URL}/api/products/${product._id}`
+          `${BASE_URL}/api/products/name/${encodeURIComponent(productName)}`
         );
         const currentProduct = resProduct.data;
         setProduct(currentProduct);
